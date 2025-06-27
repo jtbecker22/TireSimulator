d527d71fcfdaa98c86a520e9b107b81b12688dd7
jtbecker22 <jtbecker22@gmail.com>
2025-06-27 10:11:29 -0500

    Snap and Rotation feature
    Lug wrench snaps and screws off each lug wrench in order

---
+++ Assets/Scripts/SnapToLug.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SnapToLug : MonoBehaviour
+{
+    public GameObject[] interactableStages;
+    public string grabbableTag = "Grabbable"; // <- New field for tag name
+
+    private int currentStageIndex = 0;
+
+    public void TriggerNextStage(GameObject CurrentLugNut)
+    {
+        if (currentStageIndex >= interactableStages.Length) return;
+
+        GameObject current = interactableStages[currentStageIndex];
+
+        GameObject next = (currentStageIndex + 1 < interactableStages.Length)
+            ? interactableStages[currentStageIndex + 1]
+            : null;
+
+        // ✅ Change tag to allow grabbing
+        CurrentLugNut.tag = grabbableTag;
+
+        if (next != null)
+        {
+            next.SetActive(true);
+
+            
+
+            // ✅ Enable grabbing component if needed
+            var grab = CurrentLugNut.GetComponent<XRGrabInteractable>();
+            if (grab != null)
+                grab.enabled = true;
+        }
+
+        if (current != null)
+        {
+            Destroy(current);
+        }
+
+        currentStageIndex++;
+    }
+}
+
+
+
+//6 nuts
+//6 childs for each nut 
+//if wrench hit nut1, then nut1 child setActive
+//then nut1 become grabbable
+//nut2 become next trigger in the list
+//i
+
+
---
+++ Assets/Scripts/StageTrigger.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class StageTrigger : MonoBehaviour
+{
+    //[SerializeField]
+    //private SnapToLug manager;
+
+    [SerializeField]
+    private GameObject nextNut;
+
+    [SerializeField]
+    private wrenchManager managerWrench;
+
+    int index;
+
+    public bool finishRotation;
+
+    Transform parentTransform;
+    private void Start()
+    {
+        this.GetComponent<XRGrabInteractable>().enabled = false;
+        //this.GetComponent<BoxCollider>().isTrigger = true;
+       
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wrench")) 
+        {
+            //print(this.transform.name);
+            //manager.TriggerNextStage(other.gameObject);
+            //Destroy(other.transform.parent.gameObject);
+            //
+            //other.GetComponent<XRGrabInteractable>().enabled = false;
+            print(this.gameObject);
+            if (this.gameObject != managerWrench.currentNut || managerWrench.LugNuts.Count - 1 == index) { return; }
+            parentTransform = other.gameObject.transform.parent;
+            parentTransform.SetPositionAndRotation(this.transform.GetChild(0).position, this.transform.GetChild(0).rotation);
+            parentTransform.gameObject.SetActive(false);
+            //other.gameObject.transform.parent.SetActive(false);
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            //print(other.transform.parent.gameObject);
+            
+            
+            //Add rotation feature
+            //use update:
+            //if rotate
+            //then make nut grabbable
+           // this.GetComponent<XRGrabInteractable>().enabled = true;
+
+
+            /*if (nextNut != null)
+            {
+                nextNut.GetComponent<StageTrigger>().enabled = true;
+            }*/
+            //foreach (KeyValuePair<GameObject, bool> entry in managerWrench.LugNuts)
+            // {
+            //if (entry.Key == this.gameObject)
+            //{
+            //int index = managerWrench.LugNuts.IndexOf(this.gameObject);
+           // managerWrench.currentNut = managerWrench.LugNuts[index+1];
+               // }
+           // }
+            
+
+           // this.GetComponent<StageTrigger>().enabled = false;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (finishRotation) {
+
+
+            //Add rotation feature
+            //use update:
+            //if rotate
+            //then make nut grabbable
+            //this.GetComponent<XRGrabInteractable>().enabled = true;
+
+            index = managerWrench.LugNuts.IndexOf(this.gameObject);
+            //if(not null)
+            print(index);
+            print(managerWrench.LugNuts.Count);
+            if (managerWrench.LugNuts.Count - 1 > index) {
+                print("if)");
+                managerWrench.currentNut = managerWrench.LugNuts[index + 1]; ;
+                this.GetComponent<XRGrabInteractable>().enabled = true;
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                parentTransform.gameObject.SetActive(true);
+                managerWrench.enableDisableNut();
+                
+            }
+            else
+            {
+                print("else1");
+                this.GetComponent<XRGrabInteractable>().enabled = true;
+                print("else2");
+
+                this.transform.GetChild(0).gameObject.SetActive(false);
+                print("else3");
+
+                parentTransform.gameObject.SetActive(true);
+                print("else4");
+
+                this.GetComponent<StageTrigger>().enabled = false;
+                print("else5");
+
+            }
+
+
+
+        }
+    }
+
+}
