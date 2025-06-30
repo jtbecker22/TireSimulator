bc3a3819a1f6a76de4b89434651f7c5357d502e7
jtbecker22 <jtbecker22@gmail.com>
2025-06-30 09:03:39 -0500

    Minor Changes

--- Assets/Scripts/NutSnap.cs
+++ Assets/Scripts/NutSnap.cs
@@ -13,19 +13,14 @@ public class NutSnap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Nut"))
         {
             parentTransform = other.gameObject.transform.parent;
-            LugNuts[0].gameObject.SetActive(true);
-            LugNuts[1].gameObject.SetActive(true);
-            LugNuts[2].gameObject.SetActive(true);
-            LugNuts[3].gameObject.SetActive(true);
-            LugNuts[4].gameObject.SetActive(true);
-            LugNuts[5].gameObject.SetActive(true);
+            //activate one nut at a time with list, use current nut again to ensure one at a time
             parentTransform.gameObject.SetActive(false);
         }
     }
